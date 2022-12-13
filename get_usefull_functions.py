a0be5fa719940cfc9368f2c34a4b12f760ce0b2c
Tapas Sahoo <tapascuchem@gmail.com>
2022-12-13 14:41:21 +0530

    Linux: Updated on 13-12-2022

--- get_usefull_functions.py
+++ get_usefull_functions.py
@@ -3,4 +3,5 @@ def whoami():
 	print("\n" + " ATTENTION ".center(80, "*") + "\n")
 	print("%s/%s%s" %("The function is \n" + sys._getframe(1).f_code.co_filename, sys._getframe(1).f_code.co_name, "\nand the line number is " + str(sys._getframe(1).f_lineno)))
 	print ("\n" + "*"*80)
+	exit()
 
