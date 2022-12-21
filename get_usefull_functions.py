ed7c5299706ff3f85adf2922c279058ece78de51
Tapas Sahoo <tapascuchem@gmail.com>
2022-12-21 14:01:48 +0530

    Linux: Updated on 21-12-2022

--- get_usefull_functions.py
+++ get_usefull_functions.py
@@ -1,6 +1,7 @@
 import sys
+from termcolor import colored
 def whoami():
-	print("\n" + " ATTENTION ".center(80, "*") + "\n")
+	print("\n" + colored(" ATTENTION ", "green").center(80, "*") + "\n")
 	print("%s/%s%s" %("The function is \n" + sys._getframe(1).f_code.co_filename, sys._getframe(1).f_code.co_name, "\nand the line number is " + str(sys._getframe(1).f_lineno)))
 	print ("\n" + "*"*80)
 	exit()
